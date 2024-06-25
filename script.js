de73410ad65d99ab71cfa1d5a44e8e4d6991aa11
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-06-25 21:59:01 +0200

    add validations for first and last name

--- script.js
+++ script.js
@@ -6,4 +6,32 @@ const password = document.getElementById('password')
 
 formBtn.addEventListener('click', (event) => {
   event.preventDefault()
+  validateName(firstName.value, lastName.value)
+  emptyFields()
 })
+
+const emptyFields = () => {
+  if(email.value == "") {
+    console.log('its empty')
+  }
+
+  if(password.value == "") {
+    console.log('its empty')
+  }
+}
+
+const validateName = (firstName, lastName) => {
+  const namePattern = /^[a-zA-Z\s-]+$/;
+  let nameSpan = document.getElementById('nameErr')
+  let lastNameSpan = document.getElementById('lastErr')
+
+  if (!firstName || !lastName) {
+    nameSpan.innerHTML = 'First Name cannot be empty'
+    lastNameSpan.innerHTML = 'Last Name cannot be empty'
+  }
+
+  if (!namePattern.test(firstName) || !namePattern.test(lastName)) {
+    nameSpan.innerHTML = 'First name is invalid'
+    lastNameSpan.innerHTML = 'Last name is invalid'
+  }
+}
