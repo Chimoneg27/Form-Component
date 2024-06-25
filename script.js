1bd7c5ee26400b44763715b79d3fac874d14b2dc
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-06-25 20:41:59 +0200

    get input fields from index file

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const formBtn = document.getElementById('submitBtn')
+const firstName = document.getElementById('fname')
+const lastName = document.getElementById('lname')
+const email = document.getElementById('email')
+const password = document.getElementById('password')
 
 formBtn.addEventListener('click', (event) => {
   event.preventDefault()
