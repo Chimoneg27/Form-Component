49fad4198aa64a93eefa8042dc2d0adf833a120f
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-06-26 17:28:49 +0200

    remove unwanted code

--- script.js
+++ script.js
@@ -7,31 +7,15 @@ const password = document.getElementById('password')
 formBtn.addEventListener('click', (event) => {
   event.preventDefault()
   validateName(firstName.value, lastName.value)
-  emptyFields()
 })
-
-const emptyFields = () => {
-  if(email.value == "") {
-    console.log('its empty')
-  }
-
-  if(password.value == "") {
-    console.log('its empty')
-  }
-}
 
 const validateName = (firstName, lastName) => {
   const namePattern = /^[a-zA-Z\s-]+$/;
   let nameSpan = document.getElementById('nameErr')
   let lastNameSpan = document.getElementById('lastErr')
 
-  if (!firstName || !lastName) {
-    nameSpan.innerHTML = 'First Name cannot be empty'
-    lastNameSpan.innerHTML = 'Last Name cannot be empty'
-  }
-
   if (!namePattern.test(firstName) || !namePattern.test(lastName)) {
-    nameSpan.innerHTML = 'First name is invalid'
-    lastNameSpan.innerHTML = 'Last name is invalid'
+    nameSpan.innerHTML = 'First name cannot be empty'
+    lastNameSpan.innerHTML = 'Last name cannot be empty'
   }
 }
