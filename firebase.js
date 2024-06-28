86ff16b3cfa70db8adcec510c408d096bcab09be
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-06-29 00:23:56 +0200

    connect to firbase

--- firebase.js
+++ firebase.js
@@ -1,1 +1,10 @@
-import {initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js'
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js"
+
+const appSettings = {
+    databaseURL: 'https://form-component-5dd8e-default-rtdb.firebaseio.com/'
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const formComponentDB = ref(database, 'formComponent')
