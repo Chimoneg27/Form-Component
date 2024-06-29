972ba7d66ad25e7e9588fbd5209bc5e73c383fbe
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-06-29 17:46:24 +0200

    add comments

--- script.js
+++ script.js
@@ -6,10 +6,12 @@ const password = document.getElementById('password')
 let regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 const errorIcon = document.getElementsByClassName('errorIcon')
 
+// import firebase form firebase.jss
 import { firebase } from './firebase.js'
 const pushFunc = firebase.push
 const formComponentDB = firebase.formComponentDB
 
+// empty form obj to place form inputs
 let formObj = {
 }
 
@@ -19,11 +21,12 @@ formBtn.addEventListener('click', (event) => {
   validateName(firstName.value, lastName.value)
   validateEmail(email.value)
   passwordVal(password.value)
-  // pushFunc(formComponentDB, formObj)
   checkForm(formObj)
 })
 
+// this function validates the first and last name
 const validateName = (firstName, lastName) => {
+  // regex which contains only letters
   const namePattern = /^[a-zA-Z\s-]+$/;
   let nameSpan = document.getElementById('nameErr')
   let lastNameSpan = document.getElementById('lastErr')
@@ -47,10 +50,12 @@ const validateName = (firstName, lastName) => {
   }
 }
 
+// this function checks if the email is valid
 const matchRegex = (email) => {
   return email.match(regex)
 }
 
+// this function validates the email
 const validateEmail = (emailVal) => {
   let emailSpan = document.getElementById('emailErr')
   if(matchRegex(emailVal)) {
@@ -64,6 +69,7 @@ const validateEmail = (emailVal) => {
   }
 }
 
+// this function validates the password
 const passwordVal = (password) => {
   const passwordPattern = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(?!.* ).{8,16}$/
   let passwordSpan = document.getElementById('passwordErr')
@@ -78,6 +84,7 @@ const passwordVal = (password) => {
   }
 }
 
+// this function checks if the form is missing a value
 const checkForm = (obj) => {
   if(!obj.hasOwnProperty('firstName') || !obj.hasOwnProperty('lastName') || !obj.hasOwnProperty('email') || !obj.hasOwnProperty('password')) {
     console.log('Form is missing a value')
